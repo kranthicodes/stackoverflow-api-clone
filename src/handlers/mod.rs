849b30ab763f4ee27b1d2df24659a4220219a49a
Sai Kranthi <iamsaikranthi@gmail.com>
2023-02-06 01:54:28 +0530

    feat: add handlers for api

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -6,12 +6,22 @@ use crate::models::*;
 
 #[post("/question", data = "<question>")]
 pub async fn create_question(question: Json<Question>) -> Json<QuestionDetail> {
-    todo!()
+    Json(QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
 }
 
 #[get("/questions")]
 pub async fn read_questions() -> Json<Vec<QuestionDetail>> {
-    todo!()
+    Json(vec![QuestionDetail {
+        question_uuid: "question_uuid".to_owned(),
+        title: "title".to_owned(),
+        description: "description".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
 }
 
 #[delete("/question", data = "<question_uuid>")]
@@ -21,12 +31,22 @@ pub async fn delete_question(question_uuid: Json<QuestionId>) {
 
 #[post("/answer", data = "<answer>")]
 pub async fn create_answer(answer: Json<Answer>) -> Json<AnswerDetail> {
-    todo!()
+    Json(AnswerDetail {
+        answer_uuid: "Answer".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    })
 }
 
 #[get("/answers")]
 pub async fn read_answers() -> Json<Vec<AnswerDetail>> {
-    todo!()
+    Json(vec![AnswerDetail {
+        answer_uuid: "answer_uuid".to_owned(),
+        question_uuid: "question_uuid".to_owned(),
+        content: "content".to_owned(),
+        created_at: "created_at".to_owned(),
+    }])
 }
 
 #[delete("/answer", data = "<answer_uuid>")]
